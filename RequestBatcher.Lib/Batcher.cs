25ac2f6ba13be01eaa07429bfa1d26343b0bc303
Martin Komischke <martin.komischke@outlook.com>
2018-11-19 13:01:54 +0100

    introduce Batch<T> class

--- RequestBatcher.Lib/Batcher.cs
+++ RequestBatcher.Lib/Batcher.cs
@@ -3,33 +3,63 @@ using System.Collections.Generic;
 
 namespace RequestBatcher.Lib
 {
+    public class BatchIsFullException : Exception
+    {
+        public BatchIsFullException(Guid id)
+            : base($"The batch '{id}' batch is full") { }
+    }
+
+    public class Batch<T>
+    {
+        private readonly List<T> _items = new List<T>();
+        private readonly int _maxSize;
+
+        public Batch(int maxSize)
+        {
+            _maxSize = maxSize;
+        }
+
+        public IEnumerable<T> Items => _items;
+
+        public Guid Id { get; private set; } = Guid.NewGuid();
+
+        public void Add(T item)
+        {
+            if (IsFull)
+            {
+                throw new BatchIsFullException(Id);
+            }
+
+            _items.Add(item);
+        }
+
+        public bool IsFull => _maxSize == _items.Count;
+    }
+
     public class Batcher<T>
     {
-        private List<T> _batch = new List<T>();
-        private Dictionary<Guid, List<T>> _batches = new Dictionary<Guid, List<T>>();
-        private Guid _currentBatchId = Guid.NewGuid();
-        private int _maxItemsPerBatch;
+        private readonly Dictionary<Guid, Batch<T>> _batches = new Dictionary<Guid, Batch<T>>();
+        private readonly int _maxItemsPerBatch;
+        private Batch<T> _batch;
 
         public Batcher(int maxItemsPerBatch = 2)
         {
             _maxItemsPerBatch = maxItemsPerBatch;
+            _batch = new Batch<T>(_maxItemsPerBatch);
         }
 
         public Guid Add(T item)
         {
             if (IsBatchFull)
             {
-                _batches.Add(_currentBatchId, _batch);
-                _batch.Clear();
-                _currentBatchId = NextBatchId;
+                _batches.Add(_batch.Id, _batch);
+                _batch = new Batch<T>(_maxItemsPerBatch);
             }
 
             _batch.Add(item);
-            return _currentBatchId;
+            return _batch.Id;
         }
 
-        private bool IsBatchFull => _batch.Count == _maxItemsPerBatch;
-
-        private Guid NextBatchId => Guid.NewGuid();
+        private bool IsBatchFull => _batch.IsFull;
     }
 }

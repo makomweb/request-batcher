8ae1b5bc2d2bb33a298f1824cac955ad3c57ad65
Martin Komischke <martin.komischke@outlook.com>
2018-11-19 12:33:38 +0100

    implement 1st draft of the batcher

---
+++ RequestBatcher.Lib/Batcher.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace RequestBatcher.Lib
+{
+    public class Request
+    {
+
+    }
+
+    public class Batcher
+    {
+        private List<Request> _batch = new List<Request>();
+        private Dictionary<Guid, List<Request>> _batches = new Dictionary<Guid, List<Request>>();
+        private Guid _currentBatchId = Guid.NewGuid();
+        private int _maxItemsPerBatch;
+
+        public Batcher(int maxItemsPerBatch = 2)
+        {
+            _maxItemsPerBatch = maxItemsPerBatch;
+        }
+
+        public Guid Add(Request request)
+        {
+            _batch.Add(request);
+            return _currentBatchId;
+        }
+
+        private Guid NextBatchId => Guid.NewGuid();
+    }
+}
---
+++ RequestBatcher.Tests/BatcherTests.cs
@@ -0,0 +1,25 @@
+﻿using NUnit.Framework;
+using RequestBatcher.Lib;
+
+namespace RequestBatcher.Tests
+{
+    public class BatcherTests
+    {
+        [Test]
+        public void Two_subsequent_requests_should_have_the_same_id()
+        {
+            var batcher = new Batcher();
+            var g1 = batcher.Add(new Request());
+            var g2 = batcher.Add(new Request());
+
+            Assert.AreEqual(g1, g2);
+
+            var g3 = batcher.Add(new Request());
+            var g4 = batcher.Add(new Request());
+        
+            Assert.AreEqual(g3, g4);
+            Assert.AreNotEqual(g1, g3);
+        }
+        
+    }
+}

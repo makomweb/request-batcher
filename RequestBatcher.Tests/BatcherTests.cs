845fadc87a248a539d9f8c4e24cc705c47e6845f
Martin Komischke <martin.komischke@outlook.com>
2018-11-21 12:00:21 +0100

    improve tests

--- RequestBatcher.Tests/BatcherTests.cs
+++ RequestBatcher.Tests/BatcherTests.cs
@@ -10,34 +10,34 @@ namespace RequestBatcher.Tests
         [Test]
         public void Two_subsequent_requests_should_have_the_same_id()
         {
-            var batcher = new SizedBatcher<string>(_ => new BatchResponse<bool>(true), 2);
+            const int MAX_BATCH_CAPACITY = 2;
+            var batcher = new SizedBatcher<string>(_ => new BatchResponse<bool>(true), MAX_BATCH_CAPACITY);
             var one = batcher.Add("one");
             var two = batcher.Add("two");
 
-            Assert.AreEqual(one, two);
+            Assert.AreEqual(one, two, "Both requests should be handled by the same batch (identified by it's ID)!");
 
             var three = batcher.Add("three");
             var four = batcher.Add("four");
         
-            Assert.AreEqual(three, four);
-            Assert.AreNotEqual(one, three);
+            Assert.AreEqual(three, four, "Both requests should be handled by the same batch (identified by it's ID)!");
+            Assert.AreNotEqual(one, three, "There should be 2 different batches!");
         }
         
         [Test]
         public async Task If_batch_is_full_it_should_be_processed()
         {
+            const int MAX_BATCH_CAPACITY = 2;
             var batcher = new SizedBatcher<string>(batch =>
             {
                 var j = string.Join(" ", batch.Items);
                 return new BatchResponse<string>(j);
-            }, 2);
+            }, MAX_BATCH_CAPACITY);
 
             var one = batcher.Add("one");
             var two = batcher.Add("two");
-            var three = batcher.Add("three");
 
             var batchExecution = batcher.Query(one);
-            Assert.IsFalse(batchExecution.IsCompleted, "Execution should not be completed!");
 
             await batchExecution.Task;
             Assert.IsTrue(batchExecution.IsCompleted, "Execution should be completed!");
@@ -48,13 +48,38 @@ namespace RequestBatcher.Tests
         }
 
         [Test]
-        public async Task If_time_window_is_not_expired_quering_the_status_should_throw()
+        public async Task If_batch_is_NOT_full_quering_the_status_should_throw()
         {
+            const int MAX_BATCH_CAPACITY = 3;
+            var batcher = new SizedBatcher<string>(batch =>
+            {
+                var j = string.Join(" ", batch.Items);
+                return new BatchResponse<string>(j);
+            }, MAX_BATCH_CAPACITY);
+
+            var one = batcher.Add("one");
+            var two = batcher.Add("two");
+
+            try
+            {
+                var batchExecution = batcher.Query(one);
+                Assert.Fail("Quering the batch-execution should have thrown before!");
+            }
+            catch (BatchWaitingForProcessingException)
+            {
+                // Just an exception of this type is expected.
+            }
+        }
+
+        [Test]
+        public async Task If_time_window_is_NOT_expired_quering_the_status_should_throw()
+        {
+            TimeSpan MAX_BATCH_TIME_WINDOW = TimeSpan.FromSeconds(5);
             var batcher = new TimeWindowedBatcher<string>(batch =>
             {
                 var j = string.Join(" ", batch.Items);
                 return new BatchResponse<string>(j);
-            }, TimeSpan.FromSeconds(5));
+            }, MAX_BATCH_TIME_WINDOW);
 
             var one = batcher.Add("one");
             var two = batcher.Add("two");
@@ -66,19 +91,21 @@ namespace RequestBatcher.Tests
                 var batchExecution = batcher.Query(one);
                 Assert.Fail("Quering the batch-execution should have thrown before!");
             }
-            catch (BatchWaitingForProcessingException ex)
+            catch (BatchWaitingForProcessingException)
             {
+                // Just an exception of this type is expected.
             }
         }
 
         [Test]
         public async Task If_time_window_is_expired_quering_the_status_should_succeed()
         {
+            TimeSpan MAX_BATCH_TIME_WINDOW = TimeSpan.FromMilliseconds(300);
             var batcher = new TimeWindowedBatcher<string>(batch =>
             {
                 var j = string.Join(" ", batch.Items);
                 return new BatchResponse<string>(j);
-            }, TimeSpan.FromMilliseconds(300));
+            }, MAX_BATCH_TIME_WINDOW);
 
             var one = batcher.Add("one");
             var two = batcher.Add("two");
@@ -86,7 +113,7 @@ namespace RequestBatcher.Tests
             await Task.Delay(400); // ms
 
             var batchExecution = batcher.Query(one);
-            Assert.IsNotNull(batchExecution.Status);
+            Assert.AreEqual(TaskStatus.WaitingForActivation, batchExecution.Status, "Status should be 'waiting for execution'!");
         }
     }
 }
